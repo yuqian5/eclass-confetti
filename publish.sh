c5b87c3b6f38bff274b2117f07eca87fe932f951
Kerry Cao <kcyq98@gmail.com>
2023-08-14 15:27:10 -0600

    project restructured

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
-zip -r ef-firefox.zip popup res confetti.js content.js background.js manifest.json
-zip -r ef-chrome.zip popup res confetti.js content.js background.js manifest.json
+zip -r ef-firefox.zip src
+zip -r ef-chrome.zip src
